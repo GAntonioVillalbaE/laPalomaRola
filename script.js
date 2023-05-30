8a58b3fb264285013fde091350fc16980d47ab6a
Glitch (glitch-hello-website) <none>
2023-05-30 14:03:51 +0000

    🎫🚟 Checkpoint ./script.js:22109761/4602 ./sonido.html:22109761/6

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ try {
 } catch (e) {}
 
 /*  audio */
+/*
 let sonido = document.getElementById("sonido-automatico");
 
 try {
@@ -36,3 +37,30 @@ try {
     sonido.play();
   };
 } catch (e) {}
+*/
+document.onmousemove = function () {
+  /*
+  if (
+    sonidoAmbiente.paused ||
+    sonidoAmbiente.currentTime == 0 ||
+    sonidoAmbiente.currentTime == sonidoAmbiente.duration
+  ) {
+    console.log("audio no ha empezado, finalizó o está en pausa");
+    */
+    //sonidoAmbiente.play();
+  
+};
+
+const cargarSonido = function (fuente) {
+  const sonido = document.createElement("audio");
+  sonido.src = fuente;
+  sonido.setAttribute("preload", "auto");
+  sonido.setAttribute("controls", "none");
+  sonido.style.display = "none"; // <-- oculto
+  document.body.appendChild(sonido);
+  return sonido;
+};
+
+const sonidoAmbiente = cargarSonido(
+  "https://cdn.glitch.global/95c88e43-5a67-496e-903f-cd5c1b599e75/ambiente_cafeter%C3%ADa.wav?v=1684438768295"
+);
