d972256cc0897ce0ce66587e42952a5957a10cb3
Glitch (glitch-hello-website) <none>
2023-09-21 04:38:38 +0000

    ✈️👏 Checkpoint ./script.js:22109761/5:54361123/257 ./style.css:54361123/40

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ let botonCerrarVentana = document.getElementById("boton-cerrar");
 
 botonAbrirVentana.onclick = function () {
   //console.log("clic en el boton abrir");
-  ventana[0].style.backgroundColor = "red"; /* css: background-color */
+  // ventana[0].style.backgroundColor = "red"; /* css: background-color */
+  ventana[0].style.display = "block"; /*block efecto de ocupar espacio a lo ancho*/
 };
 
 /* camelCase */
+
+
+botonCerrarVentana.onclick = function () {
+  
+  ventana[0].style.display = "none";
+  
+}
