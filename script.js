429fca522a95f28472bd05c44b3964d36136feee
Glitch (glitch-hello-website) <none>
2023-09-21 20:34:33 +0000

    💂🍡 Checkpoint ./style.css:54361123/61 ./script.js:54361123/33

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-let ventana = document.getElementsByClassName("ventana-emergente");
+// let ventana = document.getElementsByClassName("ventana-emergente");
 
-let botonAbrirVentana = document.getElementById("boton-abrir");
+// let botonAbrirVentana = document.getElementById("boton-abrir");
 
-let botonCerrarVentana = document.getElementById("boton-cerrar");
+// let botonCerrarVentana = document.getElementById("boton-cerrar");
 
-botonAbrirVentana.onclick = function () {
-  //console.log("clic en el boton abrir");
-  // ventana[0].style.backgroundColor = "red"; /* css: background-color */
-  ventana[0].style.display = "block"; /*block efecto de ocupar espacio a lo ancho de la pantalla*/
-};
+// botonAbrirVentana.onclick = function () {
+//   //console.log("clic en el boton abrir");
+//   // ventana[0].style.backgroundColor = "red"; /* css: background-color */
+//   ventana[0].style.display = "block"; /*block efecto de ocupar espacio a lo ancho de la pantalla*/
+// };
 
 /* sugerencia: escribir en camelCase las variables en JS */
 
 
-botonCerrarVentana.onclick = function () {
+// botonCerrarVentana.onclick = function () {
   
-  ventana[0].style.display = "none";
+//   ventana[0].style.display = "none";
   
-}
+// }
