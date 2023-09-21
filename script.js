5920e568815f37eca3ddce0d1db4732939f4e127
Glitch (glitch-hello-website) <none>
2023-09-21 04:15:29 +0000

    🙎💒 Checkpoint ./style.css:22109761/105:54361123/2 ./Popups.html:22109761/20:54361123/31 ./script.js:22109761/375:54361123/2

---
+++ script.js
@@ -0,0 +1,10 @@
+let ventana = document.getElementsByClassName("ventana-emergente");
+
+let botonAbrirVentana = document.getElementById("boton-abrir");
+
+let botonCerrarVentana = document.getElementById("boton-cerrar");
+
+
+botonAbrirVentana.onclick = function() {
+    
+}
