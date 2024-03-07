18957e93c7f7d6ac857f89d2ea331cc92a8fbd3a
Glitch (glitch-hello-website) <none>
2024-03-07 23:44:00 +0000

    👱🤑 Checkpoint ./script.js:54361123/2916 ./Audio.html:54361123/400

--- script.js
+++ script.js
@@ -7,42 +7,30 @@ let botonCerrarVentana = document.getElementById("boton-cerrar");
 botonAbrirVentana.onclick = function () {
   //console.log("clic en el boton abrir");
   // ventana[0].style.backgroundColor = "red"; /* css: background-color */
-  ventana[0].style.display = "block"; /*block efecto de ocupar espacio a lo ancho de la pantalla*/
+  ventana[0].style.display =
+    "block"; /*block efecto de ocupar espacio a lo ancho de la pantalla*/
 };
 
 /* sugerencia: escribir en camelCase las variables en JS */
 
-
 botonCerrarVentana.onclick = function () {
-  
   ventana[0].style.display = "none";
-  
-}
+};
 
+// Para controlar audio a través de un botón
 
+// var audio = document.getElementById("Bowie-cover");
 
+// var botonPlay = document.getElementById("boton-play");
 
-// Para controlar audio a través de un botón 
-
-
-var audio = document.getElementById("Bowie-cover");
-
-var botonPlay = document.getElementById("boton-play");
-
-botonPlay.onclick  = function () {
-  audio[0].play = "play";
-}
-  
-  
-
-
+// botonPlay.onclick  = function () {
+//   audio.play();
+// }
 
 // otra forma
 
-// var audio = document.getElementById("Bowie-cover")
+function playSound() {
+  var audio = document.getElementById("Bowie-cover");
 
-// function playSound () {
-//   audio.play();
-  
-  
-// }
+  audio.play();
+}
