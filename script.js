3ed6c0080ec1b59a660d399593c02cac6c83c478
Glitch (glitch-hello-website) <none>
2024-05-14 05:10:42 +0000

    📼🚊 Checkpoint ./script.js:54361123/145

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ let botonAbrirVentana = document.getElementById("boton-abrir");
 let botonCerrarVentana = document.getElementById("boton-cerrar");
 
 botonAbrirVentana.onclick = function () {
-  //console.log("clic en el boton abrir");
+  console.log("clic en el boton abrir");
   // ventana[0].style.backgroundColor = "red"; /* css: background-color */
-  ventana[0].style.display =
+  ventana[0].style.display = /* [0] es el primer elemento que encuentra de esa clase "ventana-emergente"*/
     "block"; /*block efecto de ocupar espacio a lo ancho de la pantalla*/
 };
 
@@ -31,14 +31,14 @@ function playSound() {
 
 
 // Función para abrir el pop-up
-function abrirPopup() {
-  document.getElementById("miPopup").style.display = "block";
-}
+// function abrirPopup() {
+//   document.getElementById("miPopup").style.display = "block";
+// }
 
 // Función para cerrar el pop-up
-function cerrarPopup() {
-  document.getElementById("miPopup").style.display = "none";
-}
+// function cerrarPopup() {
+//   document.getElementById("miPopup").style.display = "none";
+// }
 
 // Asignar evento click al botón para abrir el pop-up. Esto es otra forma diferente de hacerlo: si no queremos usar dentro de nuestra etiqueta de botón el método onclick lo hacemos directamente en la hoja Javascript:
-document.getElementById("abrirPopup").addEventListener("click", abrirPopup);
+// document.getElementById("abrirPopup").addEventListener("click", abrirPopup);
